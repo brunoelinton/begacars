77eb2f6fef5c7ce472a9f68e2211da13aca570df
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-27 23:04:38 -0300

    Frontend: NavBar - hamburguer menu

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const NavBar = () => {
   return (
@@ -7,10 +8,25 @@ const NavBar = () => {
         <a href="link" className="nav-logo-text">
           <h4 className="teste">BEGA CARS</h4>
         </a>
-        <div className="collapse navbar-collapse container-main-menu">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#begacars-navbar"
+          aria-controls="begacars-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse container-main-menu" id="begacars-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">Home</a>
+              <a href="link" className="active">
+                Home
+              </a>
             </li>
             <li>
               <a href="link">Catálogo</a>
